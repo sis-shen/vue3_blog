fd832b3f7bbc508b50ad02843d394b06fa051f45
sis-shen <141546659+sis-shen@users.noreply.github.com>
2023-09-21 11:37:47 +0800

    1616

--- blog/client/src/views/dashboard/Dashboard.vue
+++ blog/client/src/views/dashboard/Dashboard.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        
+    <div class="main-panel">
+        <div class="menus">
+            <div v-for="(menu, index) in menus" @click="toPage">
+                {{ menu.name }}
+            </div>
+        </div>
+        <div style="padding:20px;width: 100% ">
+            <router-view></router-view>
+        </div>
+
     </div>
+    <div class="title">后台管理系统</div>
 </template>
 
 <script setup>
@@ -17,6 +26,51 @@ const axios = inject("axios")
 
 const adminStore = AdminStore()
 
+let menus = [
+    { name: "文章管理", href: "/dashboard/article" },
+    { name: "分类管理", href: "/dashboard/category" },
+    { name: "退出", href: "logout" },
+];
+
+const toPage = () => {
+
+}
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-panel {
+    display: flex;
+    color: #64676a;
+    max-width: 1500px;
+    margin: 0 auto;
+
+    .menus{
+        padding: 20px 0;
+        box-sizing: border-box;
+        line-height: 55px;
+        text-align: center;
+        width: 180px;
+        height: 95vh;
+        border-right: 1px solid #dadada;
+
+        div {
+            cursor: pointer; //改变鼠标指针的颜色
+    
+            &:hover {
+                color: #fd760e;
+            }
+        }
+    }
+}
+
+.title{
+    font-size: 65px;
+    font-weight: bold;
+    text-align: right;
+    position: fixed;
+    color: rgba(0,0,0,20%);
+    right: calc((100vw - 1500px)/2);
+    bottom: 20px;
+}
+</style>
