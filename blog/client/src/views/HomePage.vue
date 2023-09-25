86447e07e35f9cab0edac6955d424a50dc21dec7
sis-shen <141546659+sis-shen@users.noreply.github.com>
2023-09-25 10:12:26 +0800

    456341

--- blog/client/src/views/HomePage.vue
+++ blog/client/src/views/HomePage.vue
@@ -1,42 +1,50 @@
 <template>
     <div class="container">
-        <div class="nav">
-            <div @click="homePage"><n-button  color="rgba(255, 192, 203, 0.9)">首页</n-button></div>
-            <n-button  strong secondary type="info"  @click="dashboard" style="margin-right: 10px;">后台</n-button>
 
-            <n-popselect @update:value="searchByCategory" v-model:value="selectedCategory" :options="categoryOptions"
-                trigger="click">
-                <div>分类: <span>{{ categoryName }}</span></div>
-            </n-popselect>
-            
-    
+        <div class="cildren-container">
+            <div class="nav">
+                <div @click="homePage"><n-button color="rgba(255, 192, 203, 0.9)">首页</n-button></div>
+                <n-button strong secondary type="info" @click="dashboard" style="margin-right: 10px;">后台</n-button>
+
+                <n-popselect @update:value="searchByCategory" v-model:value="selectedCategory" :options="categoryOptions"
+                    trigger="click">
+                    <div>分类: <span>{{ categoryName }}</span></div>
+                </n-popselect>
+
+
+            </div>
+            <n-divider />
+
+            <n-space style="margin-bottom: 20px;">
+                <n-input v-model:value="pageInfo.keyword" :style="{ width: '500px' }" placeholder="请输入关键字" />
+                <n-button type="primary" ghost @click="loadBlogs(0)" @keydown.enter="loadBlogs(0)">搜索</n-button>
+            </n-space>
+
+
+            <div v-for="(blog, index) in blogListInfo" style="margin-bottom: 15px;cursor:pointer;width: 60%;">
+                <n-card :title="blog.title" @click="toDetail(blog)" class="card">
+                    {{ blog.content }}
+                    <template #footer>
+                        <n-space style="align-items: center;">
+                            <div>发布时间:{{ blog.create_time }}</div>
+                        </n-space>
+                    </template>
+                </n-card>
+            </div>
+
+            <n-pagination @update:page="loadBlogs" v-model:page="pageInfo.page" :page-count="pageInfo.pageCount" />
+
+            <n-divider />
+
         </div>
-        <n-divider />
 
-        <n-space style="margin-bottom: 20px;">
-            <n-input v-model:value="pageInfo.keyword" :style="{ width: '500px' }" placeholder="请输入关键字" />
-            <n-button type="primary" ghost @click="loadBlogs(0)" @keydown.enter="loadBlogs(0)">搜索</n-button>
-        </n-space>
+        <div class="side">
 
-
-        <div v-for="(blog, index) in blogListInfo" style="margin-bottom: 15px;cursor:pointer;width: 60%;">
-            <n-card :title="blog.title" @click="toDetail(blog)" class="card">
-                {{ blog.content }}
-                <template #footer>
-                    <n-space style="align-items: center;">
-                        <div>发布时间:{{ blog.create_time }}</div>
-                    </n-space>
-                </template>
-            </n-card>
         </div>
+    </div>
 
-        <n-pagination @update:page="loadBlogs" v-model:page="pageInfo.page" :page-count="pageInfo.pageCount" />
-
-        <n-divider />
-        <div class="footer">
-            <div>假装有版权声明</div>
-        </div>
-
+    <div class="footer">
+        <div>假装有版权声明</div>
     </div>
 </template>
 
@@ -130,8 +138,23 @@ const dashboard = () => {
 
 <style lang="scss" scoped>
 .container {
+
+    display: flex;
+
+    .side {
+        width: 300px;
+        border-left: #adadad 1px solid;
+        background: linear-gradient(#71b8ff, rgb(54, 154, 254));
+    }
+
+}
+
+.cildren-container {
     width: 1200px;
     margin: 0 auto;
+    padding-left: 150px;
+    background-image: url(https://picbed.supdriver.top/img/train.webp);
+    background-size: cover;
 }
 
 .nav {
@@ -163,6 +186,6 @@ const dashboard = () => {
 }
 
 .card {
-    background: linear-gradient(to bottom right, rgb(255, 255, 255), rgba(255, 192, 203, 0.759));
+    background: linear-gradient(to bottom right, rgb(255, 255, 255), rgba(255, 149, 0, 0.326));
 }
 </style>
